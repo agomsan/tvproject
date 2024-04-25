b6de73cb85fdb63372f6bc060a862f50ea654e07
Alex <alex@MBP-de-Manzana.home>
2024-04-25 21:03:27 +0200

    add-channels-thumbnails

--- js/main.js
+++ js/main.js
@@ -1,18 +1,108 @@
-const tvScreen = document.querySelector(".tv-screen");
-const powerSwitch = document.getElementById("power-switch");
-const volumeSwitch = document.getElementById("volume-switch");
+const buttonOnOff = document.querySelector(".onOff");
+const nameChannel = document.querySelector(".nameChannel");
+const imgHome = "./img/home.png";
+const screenTv = document.querySelector(".screenTv");
+const homeBtn = document.querySelector(".homeBtn");
+let currentChannelIndex = 0;
 
-let isTvOn = true;
-let currentVolume = 50;
+const channel = [
+  {
+    number: 1,
+    name: "DISNEY+",
+    url: "../img/disney.png",
+  },
+  {
+    number: 2,
+    name: "ESPN",
+    url: "../img/espn.png",
+  },
+  {
+    number: 3,
+    name: "FOX",
+    url: "../img/fox.png",
+  },
+  {
+    number: 4,
+    name: "HULU",
+    url: "../img/hulu.png",
+  },
+  {
+    number: 5,
+    name: "Netflix",
+    url: "../img/netflix.gif",
+  },
+  {
+    number: 6,
+    name: "HBO",
+    url: "../img/hbo.gif",
+  },
+  {
+    number: 7,
+    name: ":)",
+    url: "../img/onit.gif",
+  },
+  {
+    number: 8,
+    name: ":)",
+    url: "../img/onit.gif",
+  },
+  {
+    number: 9,
+    name: ":)",
+    url: "../img/onit.gif",
+  },
+  {
+    number: 10,
+    name: ":)",
+    url: "../img/onit.gif",
+  },
+  {
+    number: 11,
+    name: ":)",
+    url: "../img/onit.gif",
+  },
+  {
+    number: 12,
+    name: ":)",
+    url: "../img/onit.gif",
+  },
+];
 
-function togglePower() {
-  isTvOn = !isTvOn;
-  tvScreen.textContent = isTvOn ? "ON" : "OFF";
-}
+const onOffTv = () => {
+  if (screenTv.classList.contains("none")) {
+    screenTv.classList.remove("none");
+  } else {
+    screenTv.classList.add("none");
+  }
+};
 
-function setVolume() {
-  currentVolume = volumeSlider.value;
-}
+const goHome = () => {
+  if (!screenTv.classList.contains("none")) {
+    const pic = document.querySelector(".channel");
+    pic.src = imgHome;
+    nameChannel.textContent = "Home";
+    currentChannelIndex = -1;
+  }
+};
 
-powerSwitch.addEventListener("click", togglePower);
-volumeSwitch.addEventListener("input", setVolume);
+const changePic = () => {
+  const pic = document.querySelector(".channel");
+  const currentChannel = channel[currentChannelIndex];
+  if (currentChannel) {
+    pic.src = currentChannel.url;
+    nameChannel.textContent = currentChannel.name;
+  }
+};
+
+const channelUp = () => {
+  currentChannelIndex = (currentChannelIndex + 1) % channel.length;
+  changePic();
+};
+
+const channelDown = () => {
+  currentChannelIndex =
+    (currentChannelIndex - 1 + channel.length) % channel.length;
+  changePic();
+};
+
+changePic();
--- readme.md
+++ readme.md
@@ -3,10 +3,12 @@
 Este proyecto es una TV interactiva creada con HTML, CSS y JavaScript. La idea es simular una televisión virtual con un mando que permite cambiar de canal, encender y apagar la TV, y ajustar el volumen.
 
 ¿Que funciones es capaz de realizar? 📋
+Ajustar el volumen.
 Cambiar de canal.
 Encender y apagar la TV.
-Ajustar el volumen.
 Stack 🛠️
 HTML: Estructura básica.
 CSS: Estilo y medidas para la pantalla de TV y el control remoto.
-JavaScript: Se le añade la lógica para su correcto funcionamiento.</h3>
+JavaScript: Se le añade la lógica para su correcto funcionamiento.
+Google fonts: Estilos de fuente.
+</h3>
